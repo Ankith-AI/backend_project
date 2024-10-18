d71410140c199fa6faf9cb426d8ae7e553bff1dc
Sai Ankith <ankith@datalabscorp.com>
2024-10-18 13:05:10 +0530

    Updated main.py which now includes FastAPI security- user authentication.

--- main.py
+++ main.py
@@ -1,18 +1,158 @@
 from model import model_pipeline
-
 from typing import Union
-
-from fastapi import FastAPI, UploadFile
+from fastapi import Depends, FastAPI, UploadFile, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from keycloak import KeycloakOpenID
 import io
 from PIL import Image
+# import datetime
+from datetime import timedelta
+from datetime import datetime
+from pytz import timezone
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+SECRET_KEY ="86820ad5b81dd1face3e73984e343455452884ae4ed99b6f960deec7a6768949"
+ALGORITHM ="HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ 
+
 
 app = FastAPI()
+
+
+db = {
+    "ankith": {
+        "username": "ankith",
+        "full_name": "Sai Ankith P ",
+        "email": "ankith@gmail.com",
+        "hashed_password": "$2b$12$l3saj3MR/olu.0ws..26Z.EhSRYFVQbtArrZWjQyoDXu3PI78/XMS",
+        "disabled": False
+    }
+}
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None=None
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_data = db[username]
+        return UserInDB(**user_data)
+
+
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone("UTC")) + expires_delta
+    else: 
+        expire = datetime.now(timezone("UTC")) + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                         detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credential_exception
+
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credential_exception
+
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+
+    return user
+
+
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+@app.get("/users/me/items")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": 1, "owner": current_user}]
+
+
+
+# keycloak_openid = KeycloakOpenID(
+#     server_url="http://localhost:8080/",)
+#     client_id="myclient",
+#     realm_name="myrealm"
+# )
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.post("/ask")
 def ask(text: str, image: UploadFile):
@@ -24,6 +164,9 @@ def ask(text: str, image: UploadFile):
     result = model_pipeline(text, image)
     return {"answer": result}
     
-    
-    
-    
+# @app.get("/ask")
+# def read_ask():
+#     return {"ask": "page"}
+
+# pwd = get_password_hash("")
+# print(pwd)
